a5ee4bb76f7e42f5aa4a529ba424b76e447382c4
DeRon Brown <dbrown@localytics.com>
2016-11-29 18:06:59 -0500

    Update standard BroadcastReceiver integration for new 4.2 displayPushNotification method

--- standard-push-broadcastreceiver/app/src/main/java/com/localytics/android/sample/pushbroadcastreceiver/MyBroadcastReceiver.java
+++ standard-push-broadcastreceiver/app/src/main/java/com/localytics/android/sample/pushbroadcastreceiver/MyBroadcastReceiver.java
@@ -9,23 +9,22 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 
+import com.localytics.android.Localytics;
+
 public class MyBroadcastReceiver extends BroadcastReceiver
 {
     /**
      * All pushes received from Localytics will contain an 'll' string extra which can be parsed into
      * a JSON object. This JSON object contains performance tracking information, such as a campaign
      * ID. Any push received containing this 'll' string extra, should be handled by the Localytics
-     * GcmListenerService. Any other push can be handled as you see fit.
+     * SDK. Any other push can be handled as you see fit.
      */
     @Override
     public void onReceive(Context context, Intent intent)
     {
         if (intent.hasExtra("ll"))
         {
-            Intent service = new Intent(context, com.localytics.android.GcmListenerService.class);
-            service.setAction("com.google.android.c2dm.intent.RECEIVE");
-            service.putExtras(intent);
-            context.startService(service);
+            Localytics.displayPushNotification(intent.getExtras());
         }
         else
         {
